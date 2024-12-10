53d46338c4e11f34cd758a87c2c735fb7adeddeb
Ajay Sinha <ajaysinhaa09@gmail.com>
2024-12-10 23:01:45 +0530

    Working on GET files route

--- next.config.ts
+++ next.config.ts
@@ -22,6 +22,11 @@ const nextConfig: NextConfig = {
         hostname: "res.cloudinary.com",
         pathname: "**",
       },
+      {
+        protocol: "http",
+        hostname: "res.cloudinary.com",
+        pathname: "**",
+      },
     ],
   },
 };
