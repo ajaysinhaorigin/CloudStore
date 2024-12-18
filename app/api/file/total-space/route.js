ff97972a0cba79e7c0e2b30b92afe9b30f9fa0d4
Ajay Sinha <ajaysinhaa09@gmail.com>
2024-12-18 12:58:33 +0530

    minor changes

--- app/api/file/total-space/route.js
+++ app/api/file/total-space/route.js
@@ -3,54 +3,51 @@ import { verifyJWT } from "../../../../lib/middlewares/verifyJwt";
 import { utils } from "../../../../lib/utils/server-utils";
 import connectDB from "../../../../lib/dbConnection";
 import File from "../../../../lib/models/file.model";
-import mongoose from "mongoose";
 
-export const GET = asyncHandler(async (req, _) => {
-  try {
-    await connectDB();
+export const GET = asyncHandler(
+  verifyJWT(async (req, _) => {
+    try {
+      await connectDB();
 
-    // Aggregation to optimize performance
-    const totalSpace = await File.aggregate([
-      {
-        $match: {
-          owner: new mongoose.Types.ObjectId("674f3a9d2c6ac148d91c13c6"),
+      // Aggregation to optimize performance
+      const totalSpace = await File.aggregate([
+        { $match: { owner: new mongoose.Types.ObjectId(req.user._id) } },
+        {
+          $group: {
+            _id: "$type",
+            totalSize: { $sum: "$size" },
+            latestDate: { $max: "$updatedAt" },
+          },
         },
-      },
-      {
-        $group: {
-          _id: "$type",
-          totalSize: { $sum: "$size" },
-          latestDate: { $max: "$updatedAt" },
-        },
-      },
-    ]);
+      ]);
 
-    const formattedSpace = {
-      image: { size: 0, latestDate: "" },
-      document: { size: 0, latestDate: "" },
-      video: { size: 0, latestDate: "" },
-      audio: { size: 0, latestDate: "" },
-      other: { size: 0, latestDate: "" },
-      used: 0,
-      all: 1 * 1024 * 1024 * 1024,
-    };
+      const formattedSpace = {
+        image: { size: 0, latestDate: "" },
+        document: { size: 0, latestDate: "" },
+        video: { size: 0, latestDate: "" },
+        audio: { size: 0, latestDate: "" },
+        other: { size: 0, latestDate: "" },
+        used: 0,
+        all: 1 * 1024 * 1024 * 1024,
+      };
 
-    totalSpace.forEach(({ _id, totalSize, latestDate }) => {
-      formattedSpace[_id] = { size: totalSize, latestDate };
-      formattedSpace.used += totalSize;
-    });
+      totalSpace.forEach(({ _id, totalSize, latestDate }) => {
+        formattedSpace[_id] = { size: totalSize, latestDate };
+        formattedSpace.used += totalSize;
+      });
 
-    return utils.responseHandler({
-      message: "Total space fetched successfully",
-      data: { totalSpace: formattedSpace },
-      status: 200,
-      success: true,
-    });
-  } catch (error) {
-    return utils.responseHandler({
-      message: error.message || "Internal Server Error while fetching files",
-      status: error.status || 500,
-      success: false,
-    });
-  }
-});
+      return utils.responseHandler({
+        message: "Total space fetched successfully",
+        data: { totalSpace: formattedSpace },
+        status: 200,
+        success: true,
+      });
+    } catch (error) {
+      return utils.responseHandler({
+        message: error.message || "Internal Server Error while fetching files",
+        status: error.status || 500,
+        success: false,
+      });
+    }
+  })
+);
