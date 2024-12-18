3d2b0921b96558efe58ecf908c9ab83a2b3c3275
Ajay Sinha <ajaysinhaa09@gmail.com>
2024-12-18 16:47:16 +0530

    minor testing changes

--- app/api/file/total-space/route.js
+++ app/api/file/total-space/route.js
@@ -8,7 +8,7 @@ import jwt from "jsonwebtoken";
 import User from "../../../../lib/models/user.model";
 import { mongodbConfig } from "../../../../lib/dbConnection/config";
 
-export const verify = (req) => {
+const verify = (req) => {
   const token =
     req.cookies?.get("accessToken")?.value ||
     req.headers?.authorization?.replace("Bearer ", "");
@@ -21,7 +21,7 @@ export const verify = (req) => {
   }
 };
 
-export const fetchUser = async (decodedToken) => {
+const fetchUser = async (decodedToken) => {
   try {
     return await User.findById(decodedToken._id)
       .select("_id email fullName avatar")
